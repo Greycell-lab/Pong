7aef537f0864ee445b9c886facca5378bc1328fb
Greicell <dominik.severin@yandex.com>
2022-10-19 20:36:52 +0200

    Funktionable Player, Enemy and Ball Physic

--- Pong/MainWindow.xaml.cs
+++ Pong/MainWindow.xaml.cs
@@ -21,8 +21,10 @@ namespace Pong
     /// </summary>
     public partial class MainWindow : Window
     {
-        bool moveLeft, moveRight;
-        
+        bool moveLeft, moveRight, ballLeft = true, ballRight = false, ballUp = false, ballDown = true;
+        List<Rectangle> itemstoremove = new List<Rectangle>();
+        int escore = 0, pscore = 0;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -35,6 +37,8 @@ namespace Pong
         }
         void gameEngine(object sender, EventArgs e)
         {
+            //enemyScore.Content =  escore;
+            //playerScore.Content =  pscore;
             Rect playerHitbox = new Rect(Canvas.GetLeft(player), Canvas.GetTop(player), player.Width, player.Height);
             Rect enemyHitbox = new Rect(Canvas.GetLeft(enemy), Canvas.GetTop(enemy), enemy.Width, enemy.Height);
             Rect ballHitbox = new Rect(Canvas.GetLeft(ball), Canvas.GetTop(ball), ball.Width, ball.Height);
@@ -45,6 +49,54 @@ namespace Pong
             if (moveRight && Canvas.GetLeft(player) + 100 < Application.Current.MainWindow.Width)
             {
                 Canvas.SetLeft(player, Canvas.GetLeft(player) + 5);
+            }
+            if(ballLeft && Canvas.GetLeft(ball) > 5)
+            {
+                Canvas.SetLeft(ball, Canvas.GetLeft(ball) - 5);
+                if (Canvas.GetLeft(enemy) > 5)
+                {
+                    if (Canvas.GetLeft(ball) < Application.Current.MainWindow.Width - enemy.Width)
+                    {
+                        Canvas.SetLeft(enemy, Canvas.GetLeft(ball) - 5);
+                    }
+                   
+                }
+            }
+            if (ballRight && Canvas.GetLeft(ball) + 30 < Application.Current.MainWindow.Width)
+            {
+                Canvas.SetLeft(ball, Canvas.GetLeft(ball) + 5);
+                if (Canvas.GetLeft(enemy) + 100 < Application.Current.MainWindow.Width)
+                {
+                    Canvas.SetLeft(enemy, Canvas.GetLeft(ball) + 5);
+                }
+            }
+            if(Canvas.GetLeft(ball) <= 5)
+            {
+                ballLeft = false;
+                ballRight = true;
+            }
+            if (Canvas.GetLeft(ball) + 30 >= Application.Current.MainWindow.Width)
+            {
+                ballLeft = true;
+                ballRight = false;
+            }
+            if (ballUp && Canvas.GetTop(ball) > 5)
+            {
+                Canvas.SetTop(ball, Canvas.GetTop(ball) - 5);
+            }
+            if(ballDown && Canvas.GetTop(ball) + 70 < Application.Current.MainWindow.Height)
+            {
+                Canvas.SetTop(ball, Canvas.GetTop(ball) + 5);
+            }
+            if(ballHitbox.IntersectsWith(playerHitbox))
+            {
+                ballDown = false;
+                ballUp = true;
+            }
+            if(ballHitbox.IntersectsWith(enemyHitbox))
+            {
+                ballDown = true;
+                ballUp = false;
             }
             
         }
