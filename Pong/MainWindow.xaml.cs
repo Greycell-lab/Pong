cb7cd830a303a4f938a7a01c39adc6320f3c5763
Greicell <dominik.severin@yandex.com>
2022-10-19 17:06:22 +0200

    Projektdateien hinzufügen.

---
+++ Pong/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Pong
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool moveLeft, moveRight;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DispatcherTimer gameTime = new DispatcherTimer();
+            gameTime.Interval = TimeSpan.FromMilliseconds(20);
+            gameTime.Tick += gameEngine;
+            gameTime.Start();
+            MyCanvas.Focus();         
+        }
+        void gameEngine(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Left)
+            {
+                moveLeft = false;
+            }
+            if (e.Key == Key.Right)
+            {
+                moveRight = false;
+            }
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left)
+            {
+                moveLeft = true;
+            }
+        }
+    }
+}
